df524efcf28c51f3293c7b8e5e081237945bc084
koziscool <kozjob@yahoo.com>
2017-09-08 04:41:12 -0500

    update

--- 2048.js
+++ 2048.js
@@ -11,7 +11,25 @@ controller2048 = {
 };
 
 
+var handleKey = function( e ) {
+	switch( e.keyCode ){
+		case 37: // left
+			model2048.move( false, "r" );
+			break;
+		case 38: // up
+			model2048.move( false, "c" );
+			break;
+		case 39: // right
+			model2048.move( true, "r" );
+			break;
+		case 40: // down
+			model2048.move( true, "c" );
+			break;
+	}
+	view2048.updateTiles();
+};
 
 $(document).ready( function() {
 	controller2048.init();
+	window.addEventListener( 'keydown', handleKey );
 });
--- model.js
+++ model.js
@@ -6,6 +6,7 @@ model2048 = {
 	tiles: Array(16).fill(0),
 	newTiles: Array(16).fill(0),
 	probabilityNew2: 0.9,
+	gameScore: 0,
 
 	colors: [ 'lightgray','orange', 'darkkhaki', 'teal', 'limegreen', 'deepskyblue', 
       'navy', 'darkslategray', 'gray', 
@@ -18,9 +19,19 @@ model2048 = {
 
 	row: function( i ) { return Math.floor( i / this.SIDE );},
 	col: function( i ) { return i % this.SIDE; },	
+	index: function( row, col ) { return row * this.SIDE + col; },
+
 	getColor: function( value ) {
 		var index = Math.max( Math.floor(Math.log2(value)), 0);
 		return this.colors[index];
+	},
+
+	notSameBoard: function( tiles1, tiles2 ) {
+		var retBool = false;
+		for( var index in tiles1 ) {
+			retBool = retBool || ( tiles1[index] !== tiles2[index ] );
+		}
+		return retBool;
 	},
 
 	getEmptySquares: function(  ) {
@@ -41,7 +52,51 @@ model2048 = {
 		return Math.random() < this.probabilityNew2 ? 2 : 4;
 	},
 
+	stripBlanks: function( arr ) {
+		return arr.filter( function(elt){ return !!elt; } );
+	},
 
+	collapseArray: function( arr ) {
+		for( var i = 0; i < arr.length - 1; i++ ) {
+			if( !arr[i] ){
+				arr[i] = arr[i+1];
+				arr[i+1] = 0;
+			}
+
+			if( arr[i] === arr[i+1] ){
+				arr[i] *= 2;
+				this.gameScore += arr[i];
+				arr[i+1] = 0;
+			}
+		}
+	},
+
+	move: function( ascending, group_by ) {
+		
+		this.newTiles = this.tiles.slice();
+		for( var i = 0; i < this.SIDE ; i++ ) {
+			var values = [], indexes = [], k, index;
+			for( var j = 0; j < this.SIDE ; j++ ) {
+				ascending ? k = this.SIDE - j - 1 : k = j;
+				group_by === "c" ? index = this.index(k, i) : index = this.index(i, k); 
+				values.push( this.newTiles[index] );
+				indexes.push( index );
+			}
+
+			values = this.stripBlanks(values);
+			this.collapseArray(values);
+
+			for( var j = 0; j < this.SIDE ; j++ ) {
+				values[j] ? this.newTiles[ indexes[j] ] = values[j] : this.newTiles[ indexes[j] ] = 0;
+			}
+		}
+
+		if( this.notSameBoard( this.tiles, this.newTiles ) ) {
+			this.tiles = this.newTiles;
+			this.addNewSquare();
+		}
+
+	},
 
 };
 
